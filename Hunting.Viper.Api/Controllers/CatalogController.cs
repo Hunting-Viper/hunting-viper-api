9b4f8e2bc94748698c8c6f7173a650b65e90b26a
Gus Taggart <gus.taggart17@gmail.com>
2026-03-29 16:52:23 -0400

    Update CatalogController to use StoreContext for GET requests

--- Hunting.Viper.Api/Controllers/CatalogController.cs
+++ Hunting.Viper.Api/Controllers/CatalogController.cs
@@ -25,9 +25,12 @@ namespace Hunting.Viper.Api.Controllers
         [HttpGet("{id:int}")]
         public IActionResult GetItem(int id)
         {
-            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-            item.Id = id;
-            return Ok(item);
+            var item = _db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok();
         }
 
         [HttpPost]
